99d9eb2b190f218fd8359fd4094d4748b31f8fa0
QuentiinRoland <quentinroland12@gmail.com>
2025-02-21 16:39:56 +0100

    Update database configuration for Heroku

--- config/database.js
+++ config/database.js
@@ -1,21 +1,13 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Permet de contourner les erreurs de certificat auto-signé
-      },
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 module.exports = sequelize;

a4aa73b2eb5819bf2cccce5bcbe182cac772e71b
QuentiinRoland <quentinroland12@gmail.com>
2025-03-15 15:15:25 +0100

    Mise à jour de index.js cors

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,13 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Middleware
 app.use(bodyParser.json());
 
